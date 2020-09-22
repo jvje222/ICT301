916d59f5fef5a8d023ac9a44a536a6efad856fbd
Jason <jvje222@uky.edu>
2020-09-21 23:21:48 -0400

    Test

--- for_loop.php
+++ for_loop.php
@@ -10,7 +10,7 @@
   if (!empty($_GET['num']) && is_numbric ($_GET['num'])) {$num = $_GET['num'];
   echo "Your number is $num. <br>";}
   else
-  {echo "<p><ahref=for_loop_form.html>Go back and enter a number.</a></p>";
+  {echo "<p><ahref=for_loop_form.html>Go back and enter a number.</a></p>";}
 
   for ($counter=1; $counter <= $num; $counter++){echo "<p>Hello<br>";}
  ?>
