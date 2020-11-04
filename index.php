307ad06d7c6d91e266bf7d384472809010a497bc
Jason <jvje222@uky.edu>
2020-11-03 23:30:19 -0500

    8

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once ('includes/mysql_connect.php');
+$query = "SECLECT * FROM bookmarks";
+$result = @mysqli_query ($dbc, $query);
+$num  = mysqli_num_rows ($results);
+
+if ($num > 0) {
+  echo "<p> There are $sum records</p>";
+    while ($row = mysqli_fetch_array ($result, MYSQLI_ASSOC)) {
+      echo "<p>Title: $row[title] <br>";
+      echo "Comments: $row[comments] <br>";
+      echo "URL: <a href=$row[url] target=_blank>$row[url]</a></p>";
+    }
+} else {
+    echo "<p>There is no record</p>";
+}
+mysuli_close($dbc);
+ ?>
