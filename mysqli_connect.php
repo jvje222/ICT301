fad1d86684e28d524b661930f3cd545d57dcc4b4
Jason <jvje222@uky.edu>
2020-11-03 23:17:58 -0500

    8

---
+++ mysqli_connect.php
@@ -0,0 +1,11 @@
+<?php
+DEFINE ('DB_USER','jvje222')
+DEFINE ('DB_PASSWORD','4odaNzgo*')
+DEFINE ('DB_HOST','localhost')
+DEFINE ('DB_NAME','jvje222ict301')
+
+//Make connection
+
+$dbc = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD,DB_NAME)
+OR die ('could not connect to MYSQL: ' . mysqli_error())
+ ?>
