4269500839730943a9c0c7e529fe0622c1129886
Jason <jvje222@uky.edu>
2020-11-30 18:20:39 -0500

    10

--- index.php
+++ index.php
@@ -1,22 +1,44 @@
 <?php
-require_once('mysqli_connect_local.php');
+require_once ('mysqli_connect_local.php');
+?>
 
-$query = "SELECT * FROM bookmarks";
-$result = @mysqli_query($dbc, $query);
-$num = mysqli_num_rows($result);
+<form action="index.php" method="post">
+  Search: <input name="query" size="50"><input type="submit" value="search">
+</form>
 
+<?php
+if (!empty($_POST['query'])) {
+  $query = mysqli_real_escape_string($dvc, $_POST['query']);
+  $query = "SELECT * FROM bookmarks WHERE (title LIKE '%$query') OR (url LIKE '%$query') OR (comments LIKE '%$query')";
+}else {
+  $query="SELECT * FROM bookmarks";
+}
+$results = @mysqli_query ($dbc, $query);
+$num = mysqli_num_rows($results);
+if ($num>0) {
+  echo "<p>$num records are retrieved.</p>";
+  echo "<table border="1" width="80%"> <tr>
+    <td>Title</td>
+    <td>Comments</td>
+    <td>URL</td>
+    <td>Delete</td>
+    <td>Update</td>";
+
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+  echo "<tr><td>" . $row['title'] . "</td>";
+  echo "<td>" . $row['comments'] . "</td>"
+  echo "<td><a href=" . $row['url'] . "target=_blank>" . $row['url'] . "</a></td>";
+  echo "<td><a href=deleteconfirm.php?id=" . $row['id'] . ">Delete</a></td>";
+  echo "<td><a href=updateform.php?id=" . $row['id'] . ">Update</a></td></tr>";
+}
+echo "</table>";
+mysqli_free_result($result);
+
+}else {
+  echo "<p>There is no record. </p>";
+}
+echo "<p><a href=index.php>Show all records</a></p>";
 echo "<p><a href=add.php>Add a new record</a></p>";
 
-if($num > 0) {
-  echo "<p>There are $num records</p>";
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC) ) {
-      echo "<p>Title: $row[title] <br>";
-      echo "Comments: $row[comments] <br>";
-      echo "URL: <a href=$row[url] target=_blank>$row[url]</a></p>";
-      echo "<a href=deleteconfirm.php?id=$row[id]>Delete</a></p>";
-    }
-} else {
-    echo "<p>There is no record</p>";
-}
 mysqli_close($dbc);
 ?>
