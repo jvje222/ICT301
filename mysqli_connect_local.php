b485d6701abee7cd2ae7ab8a2d7055b7adcad5ce
Jason <jvje222@uky.edu>
2020-11-30 16:57:17 -0500

    test

--- mysqli_connect_local.php
+++ mysqli_connect_local.php
@@ -1,11 +1,11 @@
 <?php
 
-DEFINE ('DB_USER', 'jvje222');
-DEFINE ('DB_PASSWORD', 'eo6Eithi{ ');
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'jvje222ict301');
+DEFINE ('DB_NAME', 'test');
 
 
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die ('could not connect to MYSQL: ' . mysqli_connect_error());
-?> 
+?>
