9b2a445608af62091d7129906e00d0fb9b483085
Jason <jvje222@uky.edu>
2020-11-29 21:38:50 -0500

    8

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-DEFINE ('DB_USER', 'jvje222')
-DEFINE ('DB_PASSWORD', '4odaNzgo*')
-DEFINE ('DB_HOST', 'localhost')
-DEFINE ('DB_NAME', 'jvje222ict301')
+DEFINE ('DB_USER', 'jvje222');
+DEFINE ('DB_PASSWORD', '4odaNzgo*');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'jvje222ict301');
 
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
