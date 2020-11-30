610154ce1656c13947da89728dd02625aeccd336
Jason <jvje222@uky.edu>
2020-11-30 17:39:41 -0500

    9

--- index.php
+++ index.php
@@ -5,12 +5,15 @@ $query = "SELECT * FROM bookmarks";
 $result = @mysqli_query($dbc, $query);
 $num = mysqli_num_rows($result);
 
+echo "<p><a href=add.php>Add a new record</a></p>";
+
 if($num > 0) {
   echo "<p>There are $num records</p>";
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC) ) {
       echo "<p>Title: $row[title] <br>";
       echo "Comments: $row[comments] <br>";
       echo "URL: <a href=$row[url] target=_blank>$row[url]</a></p>";
+      echo "<a href=deleteconfirm.php?id=$row[id]Delete</a></p>";
     }
 } else {
     echo "<p>There is no record</p>";
